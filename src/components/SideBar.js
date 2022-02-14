fecaf15673d51a0863e77da7925e6be618071ade
Omkar Joshi <65808188+OmkarJ13@users.noreply.github.com>
2022-02-14 16:42:06 +0530

    Implemented responsiveness for sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -19,68 +19,62 @@ class SideBar extends React.Component {
   render() {
     const path = this.props.location.pathname;
     return (
-      <div className="fixed top-0 left-0 flex h-screen w-[15%] flex-col items-center gap-8 bg-gradient-to-br from-blue-500 to-blue-400 py-8 text-white">
-        <h1 className="flex items-center gap-1 text-2xl font-bold">
+      <div className="fixed bottom-0 left-0 flex h-[75px] w-full bg-blue-500 text-white lg:h-screen lg:w-[15%] lg:flex-col lg:py-8">
+        <h1 className="hidden lg:mb-8 lg:flex lg:items-center lg:justify-center lg:gap-1 lg:text-2xl lg:font-black">
           <AlarmOn fontSize="large" />
           Productify
         </h1>
-        <div className="flex w-full flex-col items-center gap-2">
-          <NavLink
-            to="/track"
-            exact
-            className="flex w-full items-center gap-2 px-4 py-2 text-lg  hover:bg-blue-400"
-            activeClassName="bg-blue-400"
-          >
-            {path === "/track" ? <Timer /> : <TimerOutlined />}
-            Time Tracker
-          </NavLink>
-          <NavLink
-            to="/tasks"
-            exact
-            className="flex w-full items-center gap-2 px-4 py-2 text-lg  hover:bg-blue-400"
-            activeClassName="bg-blue-400"
-          >
-            {path === "/tasks" ? <StickyNote2 /> : <StickyNote2Outlined />}
-            Tasks
-          </NavLink>
-          <span className="mt-4 self-start px-4 text-xs font-light uppercase">
-            Analyze
-          </span>
-          <NavLink
-            to="/analytics"
-            exact
-            className="flex w-full items-center gap-2 px-4 py-2 text-lg  hover:bg-blue-400"
-            activeClassName="bg-blue-400"
-          >
-            {path === "/analytics" ? <Analytics /> : <AnalyticsOutlined />}
-            Analytics
-          </NavLink>
-          <span className="mt-4 self-start px-4 text-xs font-light uppercase">
-            Manage
-          </span>
-          <NavLink
-            to="/tags"
-            exact
-            className="flex w-full items-center gap-2 px-4 py-2 text-lg  hover:bg-blue-400"
-            activeClassName="bg-blue-400"
-          >
-            {path === "/tags" ? <LocalOffer /> : <LocalOfferOutlined />}
-            Tags
-          </NavLink>
-          <NavLink
-            to="/account"
-            exact
-            className="flex w-full items-center gap-2 px-4 py-2 text-lg  hover:bg-blue-400"
-            activeClassName="bg-blue-400"
-          >
-            {path === "/account" ? (
-              <AccountCircle />
-            ) : (
-              <AccountCircleOutlined />
-            )}
-            Account
-          </NavLink>
-        </div>
+        <NavLink
+          to="/track"
+          exact
+          className="flex w-1/5 items-center justify-center gap-2 text-lg hover:bg-blue-400 lg:w-full lg:justify-start lg:px-4 lg:py-2"
+          activeClassName="bg-blue-400"
+        >
+          {path === "/track" ? <Timer /> : <TimerOutlined />}
+          <span className="hidden lg:inline">Time Tracked</span>
+        </NavLink>
+        <NavLink
+          to="/tasks"
+          exact
+          className="flex w-1/5 items-center justify-center gap-2 text-lg hover:bg-blue-400 lg:w-full lg:justify-start lg:px-4 lg:py-2"
+          activeClassName="bg-blue-400"
+        >
+          {path === "/tasks" ? <StickyNote2 /> : <StickyNote2Outlined />}
+          <span className="hidden lg:inline">Tasks</span>
+        </NavLink>
+        <span className="my-4 hidden self-start px-4 text-xs font-light uppercase lg:inline">
+          <span>Analyze</span>
+        </span>
+        <NavLink
+          to="/analytics"
+          exact
+          className="flex w-1/5 items-center justify-center gap-2 text-lg hover:bg-blue-400 lg:w-full lg:justify-start lg:px-4 lg:py-2"
+          activeClassName="bg-blue-400"
+        >
+          {path === "/analytics" ? <Analytics /> : <AnalyticsOutlined />}
+          <span className="hidden lg:inline">Analytics</span>
+        </NavLink>
+        <span className="my-4 hidden self-start px-4 text-xs font-light uppercase lg:inline">
+          <span>Manage</span>
+        </span>
+        <NavLink
+          to="/tags"
+          exact
+          className="flex w-1/5 items-center justify-center gap-2 text-lg hover:bg-blue-400 lg:w-full lg:justify-start lg:px-4 lg:py-2"
+          activeClassName="bg-blue-400"
+        >
+          {path === "/tags" ? <LocalOffer /> : <LocalOfferOutlined />}
+          <span className="hidden lg:inline">Tags</span>
+        </NavLink>
+        <NavLink
+          to="/account"
+          exact
+          className="flex w-1/5 items-center justify-center gap-2 text-lg hover:bg-blue-400 lg:w-full lg:justify-start lg:px-4 lg:py-2"
+          activeClassName="bg-blue-400"
+        >
+          {path === "/account" ? <AccountCircle /> : <AccountCircleOutlined />}
+          <span className="hidden lg:inline">Account</span>
+        </NavLink>
       </div>
     );
   }
